f9c64bbc842b40ae3f87b1fb87316b09dbf527d6
capric98 <42015599+capric98@users.noreply.github.com>
2024-04-30 18:28:15 -0500

    db test

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
+import argparse
+
 from whaleyeah import serve_config
 
 if __name__=="__main__":
-    serve_config("config.json")
+    parser = argparse.ArgumentParser(
+        prog="bot.py",
+        description="Yet another telegram bot.",
+        epilog="_(:з」∠)_",
+    )
+    parser.add_argument("-c", "--config", type=str, help="configuration json file", default="config.json")
+    args = parser.parse_args()
+
+    serve_config(args.config)
