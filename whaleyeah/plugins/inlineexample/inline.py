df230c5e29b0055b7ae4229d26afbb85e5aad9eb
capric98 <42015599+capric98@users.noreply.github.com>
2024-04-30 21:18:51 -0500

    add inline example

---
+++ whaleyeah/plugins/inlineexample/inline.py
@@ -0,0 +1,45 @@
+import logging
+
+from html import escape
+from uuid import uuid4
+
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler, ContextTypes
+from telegram.constants import ParseMode
+
+logger = logging.getLogger(__name__)
+
+def get_handler(config: dict) -> InlineQueryHandler:
+    logger.warn(DeprecationWarning("This plugin is for debug only!!!"))
+    return InlineQueryHandler(callback=inline_query)
+
+async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handle the inline query. This is run when you type: @botusername <query>"""
+    query = update.inline_query.query
+
+    if not query:  # empty query should not be handled
+        return
+
+    results = [
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="Caps",
+            input_message_content=InputTextMessageContent(query.upper()),
+        ),
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                f"<b>{escape(query)}</b>", parse_mode=ParseMode.HTML
+            ),
+        ),
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                f"<i>{escape(query)}</i>", parse_mode=ParseMode.HTML
+            ),
+        ),
+    ]
+
+    await update.inline_query.answer(results)
