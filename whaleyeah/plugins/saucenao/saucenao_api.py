132d8b327bd3d29dfe21395f422b3a4370b7d5ff
capric98 <42015599+capric98@users.noreply.github.com>
2024-05-05 19:08:33 -0500

    delete aio

--- whaleyeah/plugins/saucenao/saucenao_api.py
+++ whaleyeah/plugins/saucenao/saucenao_api.py
@@ -1,14 +1,10 @@
 from typing import Optional, BinaryIO
 
-import aiohttp
-import asyncio
 import enum
 import requests
 
-from aiohttp import ClientTimeout
 
-
-_TIMEOUT_ = 10
+_TIMEOUT_ = 5
 
 
 class DB(enum.auto):
@@ -249,7 +245,7 @@ class SauceNao:
         return self._search(params)
 
     def _search(self, params, files=None):
-        resp = requests.post(self.SAUCENAO_URL, params=params, files=files)
+        resp = requests.post(self.SAUCENAO_URL, params=params, files=files, timeout=_TIMEOUT_)
         status_code = resp.status_code
 
         if status_code == 200:
@@ -302,90 +298,6 @@ class SauceNao:
 
         return parsed_resp
 
-class AIOSauceNao(SauceNao):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self._session = None
-
-    async def __aenter__(self):
-        self._session = aiohttp.ClientSession(loop=asyncio.get_event_loop(), timeout=ClientTimeout(_TIMEOUT_))
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if self._session:
-            await self._session.close()
-
-    async def from_file(self, file: BinaryIO) -> SauceResponse:
-        return await self._search(self.params, {'file': file})
-
-    async def from_url(self, url: str) -> SauceResponse:
-        params = self.params.copy()
-        params['url'] = url
-        return await self._search(params)
-
-    async def _search(self, params, files=None):
-        session = self._session or aiohttp.ClientSession(loop=asyncio.get_event_loop(), timeout=ClientTimeout(_TIMEOUT_))
-
-        async with session.post(self.SAUCENAO_URL, params=params, data=files) as resp:
-            status_code = resp.status
-
-            # close only if not called via 'async with AIOSauceNao(...)'
-            if not self._session:
-                await session.close()
-
-            if status_code == 200:
-                parsed_resp = await resp.json()
-                raw = self._verify_response(parsed_resp, params)
-
-                return SauceResponse(raw)
-
-            # Taken from https://saucenao.com/tools/examples/api/identify_images_v1.1.py
-            # Actually server returns 200 and user_id=0 if key is bad
-            elif status_code == 403:
-                raise BadKeyError('Invalid API key')
-
-            elif status_code == 413:
-                raise BadFileSizeError('File is too large')
-
-            elif status_code == 429:
-                parsed_resp = await resp.json()
-                if 'Daily' in parsed_resp['header']['message']:
-                    raise LongLimitReachedError('24 hours limit reached')
-                raise ShortLimitReachedError('30 seconds limit reached')
-
-            raise UnknownApiError(f'Server returned status code {status_code}')
-
-    @staticmethod
-    def _verify_response(parsed_resp, params):
-        resp_header = parsed_resp['header']
-
-        status = resp_header['status']
-        user_id = int(resp_header['user_id'])
-
-        # Taken from https://saucenao.com/tools/examples/api/identify_images_v1.1.py
-        if status < 0:
-            raise UnknownClientError('Unknown client error, status < 0')
-        elif status > 0:
-            raise UnknownServerError('Unknown API error, status > 0')
-        elif user_id < 0:
-            raise UnknownServerError('Unknown API error, user_id < 0')
-
-        # Request passed, but api_key was ignored
-        elif user_id == 0 and 'api_key' in params:
-            raise BadKeyError('Invalid API key')
-
-        long_remaining = resp_header['long_remaining']
-        short_remaining = resp_header['short_remaining']
-
-        # Taken from https://saucenao.com/tools/examples/api/identify_images_v1.1.py
-        if short_remaining < 0:
-            raise ShortLimitReachedError('30 seconds limit reached')
-        elif long_remaining < 0:
-            raise LongLimitReachedError('24 hours limit reached')
-
-        return parsed_resp
 
 class SauceNaoApiError(Exception):
     pass
