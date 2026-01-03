e4657b78f092d704dca4f14aa62c28cf9d8fc97e
capric98 <42015599+capric98@users.noreply.github.com>
2026-01-03 09:57:38 -0600

    use httpx instead of requests

--- whaleyeah/jobs/bilivestalker/blive.py
+++ whaleyeah/jobs/bilivestalker/blive.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-import requests
+import httpx
 
 from telegram import Update
 from telegram.ext import MessageHandler, CallbackContext
@@ -8,15 +8,17 @@ from telegram.constants import ParseMode
 
 
 logger = logging.getLogger(__name__)
-client = requests.Session()
-client.headers.update({
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "cache-control": "no-cache",
-    "origin": "https://live.bilibili.com",
-    "pragma": "no-cache",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
-})
+client = httpx.AsyncClient(
+    headers={
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "cache-control": "no-cache",
+        "origin": "https://live.bilibili.com",
+        "pragma": "no-cache",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+    },
+    timeout=5,
+)
 
 live_info = {}
 _blive_notify_chat = []
@@ -31,7 +33,7 @@ def get_handler(config: dict):
 
 async def callback(ctx: CallbackContext):
     for k in live_info:
-        if await asyncio.to_thread(_check_live, k):
+        if await _check_live(k):
             notify_list = []
             for chat_id in _blive_notify_chat:
                 notify_list.append(ctx.bot.send_message(
@@ -41,8 +43,8 @@ async def callback(ctx: CallbackContext):
             await asyncio.gather(*notify_list)
     pass
 
-def _check_live(room_id: int) -> bool:
-    resp = client.get(f"https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id={room_id}&protocol=0,1&format=0,1,2&codec=0,1,2&qn=0&platform=web&ptype=8&dolby=5&panorama=1").json()
+async def _check_live(room_id: int) -> bool:
+    resp = (await client.get(f"https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id={room_id}&protocol=0,1&format=0,1,2&codec=0,1,2&qn=0&platform=web&ptype=8&dolby=5&panorama=1")).json()
     logger.debug(resp)
 
     if resp["code"]!=0 or not("data" in resp and "live_status" in resp["data"]):
